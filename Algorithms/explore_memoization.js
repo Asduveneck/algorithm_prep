aed740a2ef6ce3d26ee778a8a4a40d10178fc364
Asduveneck <asduveneck@ucdavis.edu>
2019-12-13 18:31:24 -0800

    Refactor fnc to console log a message and space it properly

--- Algorithms/explore_memoization.js
+++ Algorithms/explore_memoization.js
@@ -1,13 +1,3 @@
-// Testing timeEnd... is it accurate? IDK because I am trying to evaluate a promise... but not doing it well yet.
-// console.time("");
-//   setTimeout(function () { alert("Hello"); }, 3000);
-// console.timeEnd("")
-
-function factorial(n) {
-  if (n === 1) return 1;
-  return n * factorial(n - 1);
-}
-
 function benchmark(callback, name = `${callback+""}`) { // Convert callback to literal string if no name provided
   console.log(`${name}`);
   console.time("time");
@@ -15,13 +5,31 @@ function benchmark(callback, name = `${callback+""}`) { // Convert callback to l
   console.timeEnd("time");
   console.log('\n');
 }
-console.log('\n');
-console.log("#".repeat(80));
-console.log("Factorials without memo");
-console.log("#".repeat(80));
-console.log('\n');
 
-benchmark(factorial(6), "factorial(6)")  // => 720, requires 6 calls
-benchmark(factorial(6), "factorial(6) again")  // => 720, requires 6 calls
-benchmark(factorial(5), "factorial(5)")  // => 120, requires 5 calls
-benchmark(factorial(7), "factorial(7)")  // => 5040, requires 7 calls
+function consoleHeader(message = "") {
+  let newMessage = "";
+
+  if(message.length % 2 === 0) {
+    newMessage = message
+  } else {
+    newMessage = " " + message
+  }
+
+  let numSpaces = (80 - newMessage.length)/2 - 1
+  console.log('\n');
+  console.log("#".repeat(80));
+  console.log(`#${" ".repeat(numSpaces)}${newMessage}${ " ".repeat(numSpaces)}#`);
+  console.log("#".repeat(80));
+  console.log('\n');
+}
+
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+consoleHeader("Non-Memoized Factorial");
+benchmark(factorial(6), "factorial(6)");  // => 720, requires 6 calls
+benchmark(factorial(6), "factorial(6) again");  // => 720, requires 6 calls
+benchmark(factorial(5), "factorial(5)");  // => 120, requires 5 calls
+benchmark(factorial(7), "factorial(7)");  // => 5040, requires 7 calls
