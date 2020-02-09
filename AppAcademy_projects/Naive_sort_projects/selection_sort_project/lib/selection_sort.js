62de7fc47e772ca04d3963cc730db36592f6c171
Asduveneck <asduveneck@ucdavis.edu>
2020-02-08 19:07:49 -0800

    Adds in selection sort code using helper method

--- AppAcademy_projects/Naive_sort_projects/selection_sort_project/lib/selection_sort.js
+++ AppAcademy_projects/Naive_sort_projects/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,27 @@
 function swap(arr, index1, index2) {
+    let tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+    return arr;
+}
 
+const findMinIdx = (arr) => {
+    let minIdx = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[minIdx] > arr[i]) minIdx = i;
+    }
+    return minIdx;
 }
 
 function selectionSort(arr) {
 
+    for (let i = 0; i < arr.length; i++) {
+        let minIdx = i;
+        let newMinIdx = 1+i + findMinIdx(arr.slice(i+1));
+        if (arr[minIdx] > arr[newMinIdx]) swap(arr, minIdx, newMinIdx);
+    }
+    
+    return arr;
 }
 
 module.exports = {
