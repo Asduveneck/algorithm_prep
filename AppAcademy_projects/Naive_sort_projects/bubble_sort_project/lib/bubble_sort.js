77be5663b54ff16c339b99948b4fe061eb7a383f
Asduveneck <asduveneck@ucdavis.edu>
2020-02-08 17:27:47 -0800

    Complete basic project

--- AppAcademy_projects/Naive_sort_projects/bubble_sort_project/lib/bubble_sort.js
+++ AppAcademy_projects/Naive_sort_projects/bubble_sort_project/lib/bubble_sort.js
@@ -1,9 +1,22 @@
 function swap(array, idx1, idx2) {
-
+    let tmp = array[idx1]
+    array[idx1] = array[idx2];
+    array[idx2] = tmp;
+    return array;
 }
 
 function bubbleSort(array) {
-
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i+1]) {
+                swapped = true;
+                swap(array, i, i+1);
+            }
+        }
+    }
+    return array;
 }
 
 
