e6d3d677654592f0d63408ec9d87fb0ecf2b9605
Asduveneck <asduveneck@ucdavis.edu>
2020-02-09 20:29:45 -0800

    Start stack project by defining necessary components...

--- AppAcademy_projects/Data_structures/stack_project/lib/stack.js
+++ AppAcademy_projects/Data_structures/stack_project/lib/stack.js
@@ -21,11 +21,31 @@
 // -----------
 
 class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
 
+  push(val) {
+
+  }
+
+  pop() {
+
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
