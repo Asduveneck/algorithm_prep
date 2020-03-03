98fbfc50d9b18443a0f6e54fb713cfc95d0e03a8
Asduveneck <asduveneck@ucdavis.edu>
2020-03-02 23:17:16 -0800

    introduces BST search

---
+++ Structures/P_04_Binary_Search_Trees/p_02_bst_search_and_balance.md
@@ -0,0 +1,4 @@
+We use Binary Search Trees for our Binary Search algorithm (which requires a sorted array).
+And because Binary Search trees are sorted already, we should be able to implement a search function quickly.
+
+Trees are great at handling recursion, and because binary search is recursive, this binary search tree will make it easier to implement a binary search.
---
+++ Structures/P_04_Binary_Search_Trees/p_02a_bst_search.js
@@ -0,0 +1,34 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  // Previously::
+  insert(val, root = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+    if (val < root.val) {
+      if (!root.left) {
+        root.left = new TreeNode(val);
+      } else {
+        this.insert(val, root.left);
+      }
+    } else {
+      if (!root.right) {
+        root.right = new TreeNode(val);
+      } else {
+        this.insert(val, root.right);
+      }
+    }
+  }
+  // New:
+}
